190353f9c2854a396b029d32d5f04b06accad94f
4vert@k <4vert@mail.ru>
2024-02-23 02:22:48 +0900

    NULL заменен на s21_NULL

--- src/s21_memchr.c
+++ src/s21_memchr.c
@@ -2,10 +2,11 @@
 
 // возвращает указатель на первое вхождение значения c среди первых n байтов s
 // или NULL, если не найдено
-void *s21_memchr(const void *str, int c, s21_size_t n) {
+void *s21_memchr(const void *str, int c, size_t n) {
   unsigned char *char_ptr = (unsigned char *)str;
   unsigned char c_ptr = (unsigned char)c;
-  while (--n > 0 && *char_ptr++ != c_ptr) {
+  s21_size_t tmp = n;
+  while (--tmp > 0 && *char_ptr++ != c_ptr) {
     if (*char_ptr == c_ptr && n != 0) {
       return (void *)char_ptr;
       break;

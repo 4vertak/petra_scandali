d7e15a40b32af9d9f92cf60ab3b78f9f42fe90ab
4vert@k <4vert@mail.ru>
2024-02-23 23:46:11 +0900

    добавлен комментарий с описанием функции

--- src/s21_strlen.c
+++ src/s21_strlen.c
@@ -1,5 +1,7 @@
 #include "s21_string.h"
 
+// Вычисляет длину строки str, не включая завершающий нулевой символ.
+
 s21_size_t s21_strlen(const char *str) {
   int i = 0;
   while (str[i] != '\0') {

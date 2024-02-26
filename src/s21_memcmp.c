44d7db553152edeb84822b8a6f56954df74b7037
4vertak <4tvert@gmail.com>
2024-02-26 11:26:24 +0900

    Update s21_memcmp.c
    Исправил ошибку: переменную i увеличивал до проверки result == 0, что приводило к пропуску сравнения первых значений str1 и str2
    
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- src/s21_memcmp.c
+++ src/s21_memcmp.c
@@ -5,8 +5,9 @@
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
   int result = 0;
   s21_size_t i = 0;
-  while (++i < n && result == 0) {
-    result = *((char *)str1 + i) - *((char *)str2 + i);
+  while (i < n && result == 0) {
+      result = *((char *)str1 + i) - *((char *)str2 + i);
+      i++;
   }
   return result;
 }

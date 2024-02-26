cb955850d84ea12deb1646d361ef9da1a4b23b3d
4vertak <4tvert@gmail.com>
2024-02-26 12:51:23 +0900

    Update s21_strlen.c
    Отредактировал тип int i - на s21_size_t  i (функция у нас возвращает тип s21_size_t )
    
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- src/s21_strlen.c
+++ src/s21_strlen.c
@@ -3,7 +3,7 @@
 // Вычисляет длину строки str, не включая завершающий нулевой символ.
 
 s21_size_t s21_strlen(const char *str) {
-  int i = 0;
+  s21_size_t i = 0;
   while (str[i] != '\0') {
     i++;
   }

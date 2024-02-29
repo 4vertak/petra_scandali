84804f5e1fc5e09b92ca36ba8ba45f73411a5729
4vert@k <4vert@mail.ru>
2024-03-01 03:48:35 +0900

    Makefile gcov_report с lcov на gcovr

--- src/s21_strerror.c
+++ src/s21_strerror.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "s21_string.h"
 
 #ifndef S21_ERRLIST_H
@@ -7,14 +9,14 @@
 #endif
 
 char *s21_strerror(int errnum) {
-    char *res = s21_NULL;
-    static char s21_undef[BUFF_SIZE] = {'\0'}; //s21_NULL
-    static const char* const s21_errlist[S21_ERRLIST_SIZE] = {LINUX_ERRORS};
-    if (errnum < 0 || errnum >= S21_ERRLIST_SIZE) {
-        s21_sprintf(s21_undef, "Unknown error: %d", errnum);
-        res = s21_undef;
-    } else {
-        res = (char*)s21_errlist[errnum];
-    }
-    return res;
+  char *res = s21_NULL;
+  static char s21_undef[BUFF_SIZE] = {'\0'};  // s21_NULL
+  static const char *const s21_errlist[S21_ERRLIST_SIZE] = {LINUX_ERRORS};
+  if (errnum < 0 || errnum >= S21_ERRLIST_SIZE) {
+    sprintf(s21_undef, "Unknown error: %d", errnum);
+    res = s21_undef;
+  } else {
+    res = (char *)s21_errlist[errnum];
+  }
+  return res;
 }

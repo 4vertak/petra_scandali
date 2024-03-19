969bbae3a1537cedb1830f3774fb851f6fa266b3
4vertak <4tvert@gmail.com>
2024-03-19 11:26:04 +0900

    Update s21_asin.c
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- Cmath/src/s21_asin.c
+++ Cmath/src/s21_asin.c
@@ -1,24 +1,48 @@
 #include "./s21_math.h"
 
+long double s21_factorial(long double n){
+    long double fact = 1;
+    for (int c=1; c<=n; c++){
+        fact = fact * c;
+    }
+    return fact;
+}
+
+
 long double s21_asin(double x) {
   long double res = x;
   if (fabs(x) > 1) {
     res = __builtin_nan("");
-  } else if (x > S21_M_PI_4) {
-    res = acos(sqrt(1 - x * x));
-  } else if (x < -S21_M_PI_4) {
-    res = -acos(sqrt(1 - x * x));
+  } else if (x > M_PI_4) {
+    res = s21_acos(sqrt(1 - x * x));
+  } else if (x < -M_PI_4) {
+    res = -s21_acos(sqrt(1 - x * x));
   } else {
-    long double term = x;
+    long double initial_value = x;
     long double n = 1L;
-    while ((term > 0 ? term : -term) > 1E-17) {
-      term *= ((2 * n - 1) * (2 * n - 1) / ((2 * n) * (2 * n + 1))) * x * x;
-      res += term;
-      n++;
-    }
+    for (; (initial_value > 0 ? initial_value : -initial_value) > 1E-17; n++) {
+    // initial_value *= ((2 * n - 1) * (2 * n - 1) / ((2 * n) * (2 * n + 1))) * x * x;
+    initial_value = (pow(-1, n) * s21_factorial(2*n-1) * x * x) / (s21_factorial(2*n) * (2*n + 1));
+    res += initial_value;
+}
+    
   }
 
   return res;
+}
+
+long double s21_acos(double x) {
+long double result = 0;
+if (fabs(x) > 1) {
+result = __builtin_nan("");
+} else if (x > M_PI_4) {
+result = s21_asin(sqrt(1 - x * x));
+} else if (x < -M_PI_4) {
+result = M_PI - s21_asin(sqrt(1 - x * x));
+} else {
+result = M_PI_2 - s21_asin(x);
+}
+return result;
 }
 
 // int main() {

bd1930bc9832c51b7a6e94a59a7fc4c4b3098f78
4vertak <4tvert@gmail.com>
2024-03-19 14:30:49 +0900

    Update s21_fabs.c
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- Cmath/src/s21_fabs.c
+++ Cmath/src/s21_fabs.c
@@ -2,8 +2,8 @@
 
 long double s21_fabs(long double x) {
     long double result = x;
-    unsigned long long *ptr = (unsigned long long *)&result; //переопределение типа на unsigned long long для изменения битового представления числа
-    *ptr &= ~(1ULL << 63); // сброс старшего бита
-    // if (result < 0.0) result = -result; // или просто меняем знак)))
+    // unsigned long long *ptr = (unsigned long long *)&result; //переопределение типа на unsigned long long для изменения битового представления числа
+    // *ptr &= ~(1ULL << 63); // сброс старшего бита
+    if (result < 0.0) result = -result; // или просто меняем знак)))
     return result;
 }

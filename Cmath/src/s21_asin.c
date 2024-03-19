9a7a11d77202551cdd3ebfc8c2776ab668c768e2
4vertak <4tvert@gmail.com>
2024-03-19 12:56:15 +0900

    Update s21_asin.c
    Update s21_asin.c
    
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- Cmath/src/s21_asin.c
+++ Cmath/src/s21_asin.c
@@ -11,7 +11,7 @@ long double s21_asin(double x) {
   } else {
     long double initial_value = x;
     long double n = 1L;
-    for (; (initial_value > 0 ? initial_value : -initial_value) > 1E-17; n++) {
+    for (; (initial_value > 0 ? initial_value : -initial_value) > ACCURACY; n++) {
     initial_value = (pow(-1, n) * s21_factorial(2*n-1) * x * x) / (s21_factorial(2*n) * (2*n + 1)); //не забудь pow переделать
     res += initial_value;
 }

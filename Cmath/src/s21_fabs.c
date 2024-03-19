70fe7cb59b36df650fdfdf72e64daf99cf3cd738
4vertak <4tvert@gmail.com>
2024-03-19 15:41:02 +0900

    Delete Cmath/src/s21_fabs.c
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- Cmath/src/s21_fabs.c
+++
@@ -1,7 +0,0 @@
-#include "./s21_math.h"
-
-long double s21_fabs(long double x) {
-    long double result = x;
-    if (result < 0.0) result = -result; // или просто меняем знак)))
-    return result;
-}

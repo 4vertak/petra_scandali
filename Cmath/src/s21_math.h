43444256cb249bbb87e3fc3c5c5ee48f1d1524e0
4vertak <4tvert@gmail.com>
2024-03-19 12:47:58 +0900

    Update s21_math.h
    add long double s21_acos(double x)
    
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- Cmath/src/s21_math.h
+++ Cmath/src/s21_math.h
@@ -15,7 +15,7 @@ long double s21_acos(double x);
 long double s21_asin(double x);
 long double s21_atan(double x);
 // long double s21_ceil(double x);
-// long double s21_cos(double x);
+long double s21_cos(double x);
 // long double s21_exp(double x);
 // long double s21_fabs(double x);
 // long double s21_floor(double x);

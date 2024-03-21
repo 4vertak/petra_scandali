68406782666ad4c8e85138ee6f466beb42f51aff
4vertak <4tvert@gmail.com>
2024-03-21 14:50:20 +0900

    Update s21_atan.c
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- Cmath/src/s21_atan.c
+++ Cmath/src/s21_atan.c
@@ -6,40 +6,37 @@
 
 long double s21_atan(double x) {
   long double result = 0;
-  // if (s21_fabs(x) > 0.4 && s21_fabs(x) < 10) {
-    result = s21_asin(x * sqrt(1 / (1 + x * x)));  // не забудь поменять sqrt
-  
-  // смысла нет переложим ответсвенность на asin() )))
-  
-  // } else {
-  //   int dir_val = (s21_fabs(x) < 1);
-  //   if (dir_val) {
-  //     x = x;
-  //   } else {
-  //     x = 1 / x;
-  //   }
-  //   long double temp;
-  //   if (dir_val) {
-  //     temp = x;
-  //   } else {
-  //     temp = -x;
-  //   }
-  //   result = temp;
-  //   if (!dir_val) {
-  //     if (x > 0) {
-  //       result += S21_M_PI_2;
-  //     } else {
-  //       result -= S21_M_PI_2;
-  //     }
-  //   }
-  //   long double coef = 1L;
-  //   long double n = 1L;
-  //   while (s21_fabs(coef * temp) > ACCURACY && n < 20) {
-  //     coef = 1.0L / (2 * n + 1);
-  //     temp *= -x * x;
-  //     result += coef * temp;
-  //     n++;
-  //   }
-  // }
+  if (s21_fabs(x) > 0.4 && s21_fabs(x) < 10) {
+    result = s21_asin(x * sqrt(1 / (1 + x * x)));  // не забудь поменять sqrt  
+  } else {
+    int dir_val = (s21_fabs(x) < 1);
+    if (dir_val) {
+      x = x;
+    } else {
+      x = 1 / x;
+    }
+    long double temp;
+    if (dir_val) {
+      temp = x;
+    } else {
+      temp = -x;
+    }
+    result = temp;
+    if (!dir_val) {
+      if (x > 0) {
+        result += S21_M_PI_2;
+      } else {
+        result -= S21_M_PI_2;
+      }
+    }
+    long double coef = 1L;
+    long double n = 1L;
+    while (s21_fabs(coef * temp) > ACCURACY && n < 20) {
+      coef = 1.0L / (2 * n + 1);
+      temp *= -x * x;
+      result += coef * temp;
+      n++;
+    }
+  }
   return result;
 }

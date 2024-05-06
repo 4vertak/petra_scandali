a19830f1f68cf654b6bacdfcf00c6bf5b77af1d5
4vertak <4tvert@gmail.com>
2024-05-06 12:12:14 +0900

    Update s21_from_decimal_to_int.c
    modified typedef error_code
    
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- C5/src/Convertors_parsers/s21_from_decimal_to_int.c
+++ C5/src/Convertors_parsers/s21_from_decimal_to_int.c
@@ -9,12 +9,12 @@
 // (например, 0.9 преобразуется 0)
 
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
-  int result = S21_CONVERTORS_ERROR;
+  s21_convertors_error_code error_code = S21_CONVERTORS_ERROR;
   if (src.bits[1] == 0 && src.bits[2] == 0) {
     *dst = src.bits[0];
     if (get_sign(src)) *dst *= -1;
     *dst /= (int)pow(10, get_scale(src));
-    result = S21_CONVERTORS_OK;
+    error_code = S21_CONVERTORS_OK;
   }
-  return result;
+  return error_code;
 }

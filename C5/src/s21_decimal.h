96c57f65b2c1ca3d3d14d2bc6ad582d5a55b9146
4vert@k <4vert@mail.ru>
2024-05-20 23:07:02 +0900

    modified:   C5/src/Makefile 	new file:   C5/src/other_functions/s21_floor.c 	modified:   C5/src/s21_decimal.h

--- C5/src/s21_decimal.h
+++ C5/src/s21_decimal.h
@@ -118,7 +118,7 @@ typedef enum s21_another_func_error_code {
   S21_ANOTHER_FUNC_ERROR,
 } s21_another_func_error_code;
 
-// int s21_floor(s21_decimal value, s21_decimal *result);
+int s21_floor(s21_decimal value, s21_decimal *result);
 // int s21_round(s21_decimal value, s21_decimal *result);
 // int s21_truncate(s21_decimal value, s21_decimal *result);
 // int s21_negate(s21_decimal value, s21_decimal *result);

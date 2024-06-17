cf9b2ada2ccb84dea3da5f0b02b23cd1273fa9b8
4vert@k <4vert@mail.ru>
2024-06-17 22:46:49 +0900

    deleted:    SQL_beginner/Day00/Ex00/day00_ex00.sql 	deleted:    SQL_beginner/Day00/Ex01/day00_ex01.sql 	deleted:    SQL_beginner/Day00/Ex02/day00_ex02.sql 	deleted:    SQL_beginner/Day00/Ex03/day00_ex03.sql 	deleted:    SQL_beginner/Day00/Ex04/day00_ex04.sql 	deleted:    SQL_beginner/Day00/Ex05/day00_ex05.sql 	deleted:    SQL_beginner/Day00/Ex06/day00_ex06.sql 	deleted:    SQL_beginner/Day00/Ex07/day00_ex07.sql 	deleted:    SQL_beginner/Day00/Ex08/day00_ex08.sql 	deleted:    SQL_beginner/Day00/Ex09/day00_ex09.sql 	deleted:    SQL_beginner/Day00/model.sql 	deleted:    SQL_beginner/Day01/Ex00/day01_ex00.sql 	deleted:    SQL_beginner/Day01/Ex01/day01_ex01.sql 	deleted:    SQL_beginner/Day01/Ex02/day01_ex02.sql 	deleted:    SQL_beginner/Day01/Ex03/day01_ex03.sql 	deleted:    SQL_beginner/Day01/Ex04/day01_ex04.sql 	deleted:    SQL_beginner/Day01/Ex05/day01_ex05.sql 	deleted:    SQL_beginner/Day01/Ex06/day01_ex06.sql 	deleted:    SQL_beginner/Day01/Ex07/day01_ex07.sql 	deleted:    SQL_beginner/Day01/Ex08/day01_ex08.sql 	deleted:    SQL_beginner/Day01/Ex09/day01_ex09.sql 	deleted:    SQL_beginner/Day01/Ex10/day01_ex10.sql 	deleted:    SQL_beginner/Day02 	deleted:    SQL_beginner/Day03

--- SQL_beginner/Day01/Ex03/day01_ex03.sql
+++
@@ -1,8 +0,0 @@
-SELECT order_date AS action_date, person_id
-FROM person_order
-	
-INTERSECT
-	
-SELECT visit_date AS action_date, person_id
-FROM person_visits
-ORDER BY action_date ASC, person_id DESC;

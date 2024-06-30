3f73804543c87d148842e7c9eb1a817ec546b07c
4vertak <4tvert@gmail.com>
2024-06-30 14:44:28 +0900

    Update test_affine.c
    modified test case
    
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- C8_3DViewer_v1.0/src/tests/test_affine.c
+++ C8_3DViewer_v1.0/src/tests/test_affine.c
@@ -2,7 +2,8 @@
 
 START_TEST(test_translation_01) {
   char file_name[] =
-      "/mnt/c/S21/C2/petra_scandali/C8_3DViewer_v1.0/src/tests/test_object.obj";
+      "/home/woodysyl/Documents/21_project/C8_3DViewer_v1.0-1/src/tests/"
+      "test_object.obj";
   data_t data = {0};
   double test_v[] = {9.5,  -0.5, -0.5, 9.5, -0.5, 0.5,  9.5,  0.5,
                      -0.5, 9.5,  0.5,  0.5, 10.5, -0.5, -0.5, 10.5,
@@ -11,7 +12,7 @@ START_TEST(test_translation_01) {
   translation_x(&data, 10);
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = test_v[i] - data.vertex[i];
-    printf("test_translation_02\n%f == %f\n", test_v[i], data.vertex[i]);
+    // printf("test_translation_02\n%f == %f\n", test_v[i], data.vertex[i]);
     ck_assert_int_eq(test_res, 0);
   }
   free_memory_data(&data);
@@ -20,7 +21,8 @@ END_TEST
 
 START_TEST(test_translation_02) {
   char file_name[] =
-      "/mnt/c/S21/C2/petra_scandali/C8_3DViewer_v1.0/src/tests/test_object.obj";
+      "/home/woodysyl/Documents/21_project/C8_3DViewer_v1.0-1/src/tests/"
+      "test_object.obj";
   data_t data = {0};
   double test_v[] = {-0.5, 9.5,  -0.5, -0.5, 9.5,  0.5, -0.5, 10.5,
                      -0.5, -0.5, 10.5, 0.5,  0.5,  9.5, -0.5, 0.5,
@@ -29,7 +31,7 @@ START_TEST(test_translation_02) {
   translation_y(&data, 10);
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = test_v[i] - data.vertex[i];
-    printf("test_translation_02\n%f == %f\n", test_v[i], data.vertex[i]);
+    // printf("test_translation_02\n%f == %f\n", test_v[i], data.vertex[i]);
     ck_assert_int_eq(test_res, 0);
   }
   free_memory_data(&data);
@@ -38,7 +40,8 @@ END_TEST
 
 START_TEST(test_translation_03) {
   char file_name[] =
-      "/mnt/c/S21/C2/petra_scandali/C8_3DViewer_v1.0/src/tests/test_object.obj";
+      "/home/woodysyl/Documents/21_project/C8_3DViewer_v1.0-1/src/tests/"
+      "test_object.obj";
   data_t data = {0};
   double test_v[] = {-0.5, -0.5, 9.5, -0.5, -0.5, 10.5, -0.5, 0.5,
                      9.5,  -0.5, 0.5, 10.5, 0.5,  -0.5, 9.5,  0.5,
@@ -47,7 +50,7 @@ START_TEST(test_translation_03) {
   translation_z(&data, 10);
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = test_v[i] - data.vertex[i];
-    printf("test_translation_03\n%f == %f\n", test_v[i], data.vertex[i]);
+    // printf("test_translation_03\n%f == %f\n", test_v[i], data.vertex[i]);
     ck_assert_int_eq(test_res, 0);
   }
   free_memory_data(&data);
@@ -56,19 +59,70 @@ END_TEST
 
 START_TEST(test_rotation_01) {
   char file_name[] =
-      "/mnt/c/S21/C2/petra_scandali/C8_3DViewer_v1.0/src/tests/test_object.obj";
+      "/home/woodysyl/Documents/21_project/C8_3DViewer_v1.0-1/src/tests/"
+      "test_object.obj";
   data_t data = {0};
-  double test_v[] = {-0.5, 0.5,  -0.5, -0.5, -0.5, -0.5, -0.5, 0.5,
-                     0.5,  -0.5, -0.5, 0.5,  0.5,  0.5,  -0.5, 0.5,
-                     -0.5, -0.5, 0.5,  0.5,  0.5,  0.5,  -0.5, 0.5};
+  double test_v[] = {-0.500000, -0.486106, 0.486106,  -0.500000, -0.513518,
+                     -0.513518, -0.500000, 0.513518,  0.513518,  -0.500000,
+                     0.486106,  -0.486106, 0.500000,  -0.486106, 0.486106,
+                     0.500000,  -0.513518, -0.513518, 0.500000,  0.513518,
+                     0.513518,  0.500000,  0.486106,  -0.486106};
 
   int res = loader(&data, file_name);
   rotation_x(&data, M_PI / 2);
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = test_v[i] - data.vertex[i];
-    printf("test_rotation_01\n%f == %f\n", test_v[i], data.vertex[i]);
+    // printf("%f, ", data.vertex[i]);
     ck_assert_int_eq(test_res, 0);
-    // ck_assert_double_eq_tol(test_v[i], data.vertex[i], 0.000001);
+    ck_assert_double_eq_tol(test_v[i], data.vertex[i], 0.000001);
+  }
+
+  free_memory_data(&data);
+}
+END_TEST
+
+START_TEST(test_rotation_02) {
+  char file_name[] =
+      "/home/woodysyl/Documents/21_project/C8_3DViewer_v1.0-1/src/tests/"
+      "test_object.obj";
+  data_t data = {0};
+  double test_v[] = {-0.486106, -0.500000, 0.486106,  -0.513518, -0.500000,
+                     -0.513518, -0.486106, 0.500000,  0.486106,  -0.513518,
+                     0.500000,  -0.513518, 0.513518,  -0.500000, 0.513518,
+                     0.486106,  -0.500000, -0.486106, 0.513518,  0.500000,
+                     0.513518,  0.486106,  0.500000,  -0.486106};
+
+  int res = loader(&data, file_name);
+  rotation_y(&data, M_PI / 2);
+  for (int i = 0; i < data.count_vertex * 3; i++) {
+    int test_res = test_v[i] - data.vertex[i];
+    // printf("%f, ", data.vertex[i]);
+    ck_assert_int_eq(test_res, 0);
+    ck_assert_double_eq_tol(test_v[i], data.vertex[i], 0.000001);
+  }
+
+  free_memory_data(&data);
+}
+END_TEST
+
+START_TEST(test_rotation_03) {
+  char file_name[] =
+      "/home/woodysyl/Documents/21_project/C8_3DViewer_v1.0-1/src/tests/"
+      "test_object.obj";
+  data_t data = {0};
+  double test_v[] = {-0.486106, 0.486106,  -0.500000, -0.486106, 0.486106,
+                     0.500000,  -0.513518, -0.513518, -0.500000, -0.513518,
+                     -0.513518, 0.500000,  0.513518,  0.513518,  -0.500000,
+                     0.513518,  0.513518,  0.500000,  0.486106,  -0.486106,
+                     -0.500000, 0.486106,  -0.486106, 0.500000};
+
+  int res = loader(&data, file_name);
+  rotation_z(&data, M_PI / 2);
+  for (int i = 0; i < data.count_vertex * 3; i++) {
+    int test_res = test_v[i] - data.vertex[i];
+    // printf("%f, ", data.vertex[i]);
+    ck_assert_int_eq(test_res, 0);
+    ck_assert_double_eq_tol(test_v[i], data.vertex[i], 0.000001);
   }
 
   free_memory_data(&data);
@@ -77,24 +131,25 @@ END_TEST
 
 START_TEST(test_scaling_01) {
   char file_name[] =
-      "/mnt/c/S21/C2/petra_scandali/C8_3DViewer_v1.0/src/tests/test_object.obj";
+      "/home/woodysyl/Documents/21_project/C8_3DViewer_v1.0-1/src/tests/"
+      "test_object.obj";
   data_t data = {0};
 
   double test_v[] = {-1.5, -1.5, -1.5, -1.5, -1.5, 1.5,  -1.5, 1.5,
                      -1.5, -1.5, 1.5,  1.5,  1.5,  -1.5, -1.5, 1.5,
                      -1.5, 1.5,  1.5,  1.5,  -1.5, 1.5,  1.5,  1.5};
   int res = loader(&data, file_name);
-  printf("загрузка\n");
-  for (int i = 0; i < data.count_vertex * 3; i++) {
-    printf("%f, ", data.vertex[i]);
-  }
+  // printf("загрузка\n");
+  // for (int i = 0; i < data.count_vertex * 3; i++) {
+  //   printf("%f, ", data.vertex[i]);
+  // }
   scaling(&data, 3);
-  printf("scaling\n");
+  // printf("scaling\n");
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = data.vertex[i] - test_v[i];
-    printf("%f == %f\n", test_v[i], data.vertex[i]);
+    // printf("%f == %f\n", test_v[i], data.vertex[i]);
     ck_assert_int_eq(test_res, 0);
-    // ck_assert_double_eq(test_v[i], data.vertex[i]);
+    ck_assert_double_eq(test_v[i], data.vertex[i]);
   }
 
   free_memory_data(&data);
@@ -111,6 +166,8 @@ Suite *affine_transformations(void) {
   tcase_add_test(tc, test_translation_02);
   tcase_add_test(tc, test_translation_03);
   tcase_add_test(tc, test_rotation_01);
+  tcase_add_test(tc, test_rotation_02);
+  tcase_add_test(tc, test_rotation_03);
   tcase_add_test(tc, test_scaling_01);
 
   return s;

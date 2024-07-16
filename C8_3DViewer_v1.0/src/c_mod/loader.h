4a810a1b3cd8322c3975a4594fa46c93a213a30c
4vertak <4tvert@gmail.com>
2024-07-16 10:36:16 +0900

    Update loader.h
    add s21_getline func
    
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- C8_3DViewer_v1.0/src/c_mod/loader.h
+++ C8_3DViewer_v1.0/src/c_mod/loader.h
@@ -1,14 +1,15 @@
 #ifndef LOADER_H_
 #define LOADER_H_
 
-#define _GNU_SOURCE
-
 #include <float.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+
+#define INITIAL_SIZE 128
 
 typedef struct {
   unsigned count_vertex;
@@ -30,6 +31,8 @@ int loader(data_t* data, const char* file_name);
 int handle_data(FILE* f, data_t* data);
 void calc_count(FILE* f, data_t* data);
 void center_position(data_t* data, min_max_t* min_max);
+
+size_t s21_getline(char **lineptr, size_t *n, FILE *stream);
 
 int allocate_memory_data(data_t* data);
 void free_memory_data(data_t* data);

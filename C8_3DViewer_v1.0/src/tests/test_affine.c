cd5a45fb53c2e5562d3d3c2c95139fe5458335e1
4vertak <4tvert@gmail.com>
2024-07-19 14:56:36 +0900

    Update test_affine.c
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- C8_3DViewer_v1.0/src/tests/test_affine.c
+++ C8_3DViewer_v1.0/src/tests/test_affine.c
@@ -12,7 +12,10 @@ START_TEST(test_translation_01) {
   x_offset(&data, 10);
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = test_v[i] - data.vertex[i];
-    // printf("test_translation_02\n%f == %f\n", test_v[i], data.vertex[i]);
+#ifdef DEBAG
+    if (test_v[i] - data.vertex[i] < 0.000001) printf(GREEN "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+    else  printf(RED "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+#endif
     ck_assert_int_eq(test_res, 0);
   }
   free_memory_data(&data);
@@ -31,7 +34,10 @@ START_TEST(test_translation_02) {
   y_offset(&data, 10);
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = test_v[i] - data.vertex[i];
-    // printf("test_translation_02\n%f == %f\n", test_v[i], data.vertex[i]);
+#ifdef DEBAG
+    if (test_v[i] - data.vertex[i] < 0.000001) printf(GREEN "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+    else  printf(RED "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+#endif
     ck_assert_int_eq(test_res, 0);
   }
   free_memory_data(&data);
@@ -50,7 +56,10 @@ START_TEST(test_translation_03) {
   z_offset(&data, 10);
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = test_v[i] - data.vertex[i];
-    // printf("test_translation_03\n%f == %f\n", test_v[i], data.vertex[i]);
+#ifdef DEBAG
+    if (test_v[i] - data.vertex[i] < 0.000001) printf(GREEN "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+    else  printf(RED "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+#endif
     ck_assert_int_eq(test_res, 0);
   }
   free_memory_data(&data);
@@ -61,11 +70,8 @@ START_TEST(test_rotation_01) {
   char file_name[] =
       "/mnt/c/S21/C2/petra_scandali/C8_3DViewer_v1.0/src/tests/test_object.obj";
   data_t data = {0};
-  double test_v[] = {-0.500000, -0.486106, 0.486106,  -0.500000, -0.513518,
-                     -0.513518, -0.500000, 0.513518,  0.513518,  -0.500000,
-                     0.486106,  -0.486106, 0.500000,  -0.486106, 0.486106,
-                     0.500000,  -0.513518, -0.513518, 0.500000,  0.513518,
-                     0.513518,  0.500000,  0.486106,  -0.486106};
+  double test_v[] = {-0.500000, 0.500000, -0.500000, -0.500000, -0.500000, -0.500000, -0.500000, 0.500000, 0.500000, -0.500000, -0.500000, 0.500000,
+                       0.500000,  0.500000, -0.500000, 0.500000,  -0.500000, -0.500000, 0.500000,  0.500000, 0.500000, 0.500000,  -0.500000, 0.500000};
 
   int res = loader(&data, file_name);
   ck_assert_int_eq(res, 0);
@@ -74,7 +80,10 @@ START_TEST(test_rotation_01) {
     int test_res = test_v[i] - data.vertex[i];
     // printf("%f, ", data.vertex[i]);
     ck_assert_int_eq(test_res, 0);
-    // ck_assert_double_eq_tol(test_v[i], data.vertex[i], 0.000001);
+#ifdef DEBAG
+    if (test_v[i] - data.vertex[i] < 0.000001) printf(GREEN "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+    else  printf(RED "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+#endif
   }
 
   free_memory_data(&data);
@@ -85,18 +94,21 @@ START_TEST(test_rotation_02) {
   char file_name[] =
       "/mnt/c/S21/C2/petra_scandali/C8_3DViewer_v1.0/src/tests/test_object.obj";
   data_t data = {0};
-  double test_v[] = {-0.486106, -0.500000, 0.486106,  -0.513518, -0.500000,
-                     -0.513518, -0.486106, 0.500000,  0.486106,  -0.513518,
-                     0.500000,  -0.513518, 0.513518,  -0.500000, 0.513518,
-                     0.486106,  -0.500000, -0.486106, 0.513518,  0.500000,
-                     0.513518,  0.486106,  0.500000,  -0.486106};
+  double test_v[] = {-0.500000, -0.500000, 0.500000,  -0.500000, -0.500000,
+                     -0.500000, -0.500000, 0.500000,  0.500000,  -0.500000,
+                     0.500000,  -0.500000, -0.500000,  -0.500000, -0.500000,
+                     0.500000,  -0.500000, -0.500000, -0.500000,  0.500000,
+                    -0.500000,  0.500000,  0.500000,  -0.500000};
 
   int res = loader(&data, file_name);
   ck_assert_int_eq(res, 0);
   rotation_y(&data, M_PI / 2);
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = test_v[i] - data.vertex[i];
-    // printf("%f, ", data.vertex[i]);
+#ifdef DEBAG
+    if (test_v[i] - data.vertex[i] < 0.000001) printf(GREEN "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+    else  printf(RED "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+#endif
     ck_assert_int_eq(test_res, 0);
     // ck_assert_double_eq_tol(test_v[i], data.vertex[i], 0.000001);
   }
@@ -109,18 +121,21 @@ START_TEST(test_rotation_03) {
   char file_name[] =
       "/mnt/c/S21/C2/petra_scandali/C8_3DViewer_v1.0/src/tests/test_object.obj";
   data_t data = {0};
-  double test_v[] = {-0.486106, 0.486106,  -0.500000, -0.486106, 0.486106,
-                     0.500000,  -0.513518, -0.513518, -0.500000, -0.513518,
-                     -0.513518, 0.500000,  0.513518,  0.513518,  -0.500000,
-                     0.513518,  0.513518,  0.500000,  0.486106,  -0.486106,
-                     -0.500000, 0.486106,  -0.486106, 0.500000};
+  double test_v[] = {-0.500000, 0.500000,  -0.500000, -0.500000, -0.500000,
+                     -0.500000,  -0.500000, -0.500000, -0.500000, -0.500000,
+                     -0.500000, 0.500000,  0.500000,  0.500000,  -0.500000,
+                     0.500000,  -0.500000,  -0.500000,  0.500000,  -0.500000,
+                     -0.500000, 0.500000,  -0.500000, 0.500000};
 
   int res = loader(&data, file_name);
   ck_assert_int_eq(res, 0);
   rotation_z(&data, M_PI / 2);
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = test_v[i] - data.vertex[i];
-    // printf("%f, ", data.vertex[i]);
+#ifdef DEBAG
+    if (test_v[i] - data.vertex[i] < 0.000001) printf(GREEN "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+    else  printf(RED "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+#endif
     ck_assert_int_eq(test_res, 0);
     // ck_assert_double_eq_tol(test_v[i], data.vertex[i], 0.000001);
   }
@@ -147,7 +162,10 @@ START_TEST(test_scaling_01) {
   // printf("scaling\n");
   for (int i = 0; i < data.count_vertex * 3; i++) {
     int test_res = data.vertex[i] - test_v[i];
-    // printf("%f == %f\n", test_v[i], data.vertex[i]);
+#ifdef DEBAG
+    if (test_v[i] - data.vertex[i] < 0.000001) printf(GREEN "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+    else  printf(RED "test_rotation\n%f == %f\n", test_v[i], data.vertex[i]);
+#endif
     ck_assert_int_eq(test_res, 0);
     // ck_assert_double_eq(test_v[i], data.vertex[i]);
   }
@@ -158,7 +176,7 @@ END_TEST
 
 Suite *affine_transformations(void) {
   Suite *s =
-      suite_create("\n\033[37;1m==========| AFFINE FUNC |=========\033[0m");
+      suite_create(ORANGE "\n==========| AFFINE FUNC |=========");
   TCase *tc = tcase_create("tc_affine_func");
 
   suite_add_tcase(s, tc);

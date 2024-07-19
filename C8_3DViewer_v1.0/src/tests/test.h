96145dd30621f13382689c30658876ff3af7dbc6
4vertak <4tvert@gmail.com>
2024-07-19 14:41:28 +0900

    Update test.h
    add
    #define RED "\033[0;31m"
    #define ORANGE "\033[1;33m"
    #define GREEN "\033[0;32m"
    
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- C8_3DViewer_v1.0/src/tests/test.h
+++ C8_3DViewer_v1.0/src/tests/test.h
@@ -6,6 +6,10 @@
 #include "../c_mod/affine_func.h"
 #include "../c_mod/loader.h"
 
+#define RED "\033[0;31m"
+#define ORANGE "\033[1;33m"
+#define GREEN "\033[0;32m"
+
 // загрузчик:
 Suite *test_loader(void);
 Suite *affine_transformations(void);

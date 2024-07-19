544b97181c3351a3f32915975468e8ae011eda30
4vertak <4tvert@gmail.com>
2024-07-19 10:22:45 +0900

    Update rotation_y.c
    1. Исправлена ошибка вычисления координат x и z;
    2. Добавлена проверка *data.
    
    Signed-off-by: 4vertak <4tvert@gmail.com>

--- C8_3DViewer_v1.0/src/c_mod/affine_func/rotation_y.c
+++ C8_3DViewer_v1.0/src/c_mod/affine_func/rotation_y.c
@@ -5,15 +5,17 @@
  *
  * @param data Указатель на структуру данных, содержащую массив вершин
  * @param value Угол поворота в градусах
+ * to-do: решить что будем передавать градусы или радианы?
  */
 void rotation_y(data_t* data, double value) {
+  if (data == NULL || data->vertex == NULL) {
+        return; // Проверка на NULL
+    }
   size_t num_vertices = data->count_vertex;
   for (size_t i = 0; i < 3 * num_vertices; i += 3) {
     double tmp_x = data->vertex[i];
     double tmp_z = data->vertex[i + 2];
-    data->vertex[i] =
-        cos(value * (M_PI / 180)) * tmp_x - sin(value * (M_PI / 180)) * tmp_z;
-    data->vertex[i + 2] =
-        sin(value * (M_PI / 180)) * tmp_x - cos(value * (M_PI / 180)) * tmp_z;
+    data->vertex[i] = cos(value * (M_PI / 180)) * tmp_x + sin(value * (M_PI / 180)) * tmp_z; // Изменено
+    data->vertex[i + 2] = -sin(value * (M_PI / 180)) * tmp_x + cos(value * (M_PI / 180)) * tmp_z; // Изменено
   }
 }

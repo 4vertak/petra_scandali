7da6e4328e943013df2426369c2075b150dfcdd9
4vertak <4tvert@gmail.com>
2025-01-29 17:20:04 +0900

    Create create_dir_and_files_for_Linux_Monitoring_v1.0.sh
    add create_dir_and_files_for_Linux_Monitoring_v1.0.sh
    
    Signed-off-by: 4vertak <4tvert@gmail.com>

---
+++ DO3_LinuxMonitoring/create_dir_and_files_for_Linux_Monitoring_v1.0.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo''
+line="Let's start creating a directory and files to turn it."
+len=${#line}
+for ((i=0; i<len; i++)); do
+    printf "\e[1;33m${line:$i:1}\e[0m"
+    sleep 0.1
+done
+echo''
+echo''
+
+while true; do
+    read -p "Write the relative path ('./src/' or './'):" path
+    if [ -d "$path" ]; then
+        break
+    else
+        echo "No such directory"
+        continue
+    fi
+done
+
+for ((i=1; i<6; i++))
+do
+    mkdir "$path"0$i
+    touch "$path"0$i/main.sh
+done
+
+echo''
+line="And may the Bash-Force be with you!"
+len=${#line}
+for ((i=0; i<len; i++)); do
+    printf "\e[1;33m${line:$i:1}\e[0m"
+    sleep 0.1
+done
+echo ''
+echo ''
